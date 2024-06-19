ea4fcee663fc466f3f1396e4ca56cda008da9fa5
EmanElkafrawy <emanelkafrawy2018@gmail.com>
2024-06-20 01:46:45 +0300

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using firstProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace firstProject.Data
+{
+	public class AppDbContext: DbContext
+    { //constructor
+		//here to initialize your DBdb connection
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+		{
+
+		}
+		public DbSet<Item> Items { get; set; }
+	}
+}
+
